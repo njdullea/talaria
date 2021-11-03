bb8739d9660a95f8f037f3de7a943a02779656e1
Nathan Dullea <njdullea@gmail.com>
2021-11-03 16:58:28 -0600

    reading csv data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs;
 use ta::indicators::ExponentialMovingAverage;
 use ta::Next;
+
+#[derive(Debug, Deserialize, Clone)]
+struct Record {
+    date: String,
+    open: f64,
+    close: f64,
+	high: f64,
+    low: f64,
+	volume: f64,
+	adj_close: f64,
+}
 
 fn main() {
     let mut ema = ExponentialMovingAverage::new(3).unwrap();
@@ -8,4 +22,29 @@ fn main() {
     ema.next(1.0);
     let avg = ema.next(6.25);
     println!("Avg: {:?}", avg);
+    match print_csv_data("data/AMZN.csv") {
+        Ok(_) => println!("Ok"),
+        Err(_) => println!("Err"),
+    }
+}
+
+fn print_csv_data(filename: &str) -> Result<(), Box<dyn Error>> {
+	let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
+    let mut rdr = csv::Reader::from_reader(contents.as_bytes());
+
+	// let mut data_points = Vec::new();
+
+    for record in rdr.deserialize() {
+        let record: Record = record?;
+        println!("Record: {:?}", record);
+        // let data_point = DataPoint {
+        //     open: record.open * 1000_f64,
+        //     close: record.close * 1000_f64,
+        //     volume: record.volume as f64,
+        // };
+
+		// data_points.insert(0, data_point);
+    };
+
+	Ok(())
 }

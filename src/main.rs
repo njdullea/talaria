36fbef41b63871ccfe18109f51bf2ea26d44ec9a
Nathan Dullea <njdullea@gmail.com>
2021-11-05 19:09:08 -0600

    refactored backtest function to accept type with trader trait

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
 use std::error::Error;
 use std::fs;
-use ta::indicators::{RelativeStrengthIndex};
-use ta::Next;
+use ta::indicators::RelativeStrengthIndex;
+use ta::DataItem;
+use ta::{Close, Next};
 
 mod record;
 use crate::record::Record;
 
+trait Trade {
+    fn trade(&mut self, data_item: DataItem) -> Option<MarketAction>;
+}
+
 #[derive(Debug)]
-enum MarketActionType {
+enum MarketAction {
     Buy,
-    Sell
+    Sell,
 }
 
 fn main() {
-    match backtest_rsi() {
+    // match backtest_rsi() {
+    //     Ok(_) => println!("Ok"),
+    //     Err(_) => println!("Err"),
+    // }
+
+    let mut rsi_trader = RSITrader::new(14).unwrap();
+    match backtest(&mut rsi_trader) {
         Ok(_) => println!("Ok"),
         Err(_) => println!("Err"),
     }
 }
 
-fn backtest_rsi() -> Result<(), Box<dyn Error>> {
-	let contents = fs::read_to_string("data/AMZN.csv").expect("Something went wrong reading the file.");
+fn backtest(mut trader: impl Trade) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_to_string("data/AMZN.csv").expect("Something went wrong reading the file.");
     let mut rdr = csv::Reader::from_reader(contents.as_bytes());
-    let mut rsi_trader = RSITrader::new(14).unwrap();
-    
-    // Total amount of stock currently owned.
     let mut stock_qty = 0.0;
-    // Amount to stock to trade during each exchange.
     let trade_qty = 2.0;
-    // Total amount of fiat money that can be spent during each exchange.
     let mut fiat_total = 10000.0;
     println!("Starting stock qty: {:?}", stock_qty);
     println!("Starting fiat_total: {:?}", fiat_total);
@@ -36,33 +43,85 @@ fn backtest_rsi() -> Result<(), Box<dyn Error>> {
     for record in rdr.deserialize() {
         let record: Record = record?;
 
-        match rsi_trader.next(record.close) {
+        let data_item = DataItem::builder()
+            .close(record.close)
+            .open(record.open)
+            .volume(record.volume)
+            .high(record.high)
+            .low(record.low)
+            .build()
+            .unwrap();
+
+        match trader.trade(data_item) {
             Some(action) => {
                 println!("Time to {:?}", action);
                 match action {
-                    MarketActionType::Buy => {
+                    MarketAction::Buy => {
                         let cost = trade_qty * record.close;
                         fiat_total = fiat_total - cost;
                         stock_qty = stock_qty + trade_qty;
-                    },
-                    MarketActionType::Sell => {
+                    }
+                    MarketAction::Sell => {
                         let cost = trade_qty * record.close;
                         fiat_total = fiat_total + cost;
                         stock_qty = stock_qty - trade_qty;
-                    },
+                    }
                 }
-            println!("New stock qty: {:?}", stock_qty);
-            println!("New fiat_total: {:?}", fiat_total);
-            },
+            }
             None => (),
         }
-    };
+    }
 
     println!("Ending stock qty: {:?}", stock_qty);
     println!("Ending fiat_total: {:?}", fiat_total);
 
-	Ok(())
+    Ok(())
 }
+
+// fn backtest_rsi() -> Result<(), Box<dyn Error>> {
+// 	let contents = fs::read_to_string("data/AMZN.csv").expect("Something went wrong reading the file.");
+//     let mut rdr = csv::Reader::from_reader(contents.as_bytes());
+//     let mut rsi_trader = RSITrader::new(14).unwrap();
+
+//     // Total amount of stock currently owned.
+//     let mut stock_qty = 0.0;
+//     // Amount to stock to trade during each exchange.
+//     let trade_qty = 2.0;
+//     // Total amount of fiat money that can be spent during each exchange.
+//     let mut fiat_total = 10000.0;
+//     println!("Starting stock qty: {:?}", stock_qty);
+//     println!("Starting fiat_total: {:?}", fiat_total);
+
+//     for record in rdr.deserialize() {
+//         let record: Record = record?;
+
+//         match rsi_trader.next(record.close) {
+//             Some(action) => {
+//                 println!("Time to {:?}", action);
+//                 match action {
+//                     MarketAction::Buy => {
+//                         let cost = trade_qty * record.close;
+//                         fiat_total = fiat_total - cost;
+//                         stock_qty = stock_qty + trade_qty;
+//                     },
+//                     MarketAction::Sell => {
+//                         let cost = trade_qty * record.close;
+//                         fiat_total = fiat_total + cost;
+//                         stock_qty = stock_qty - trade_qty;
+//                     },
+//                 }
+//             println!("New stock qty: {:?}", stock_qty);
+//             println!("New fiat_total: {:?}", fiat_total);
+//             },
+//             None => (),
+//         }
+//     };
+
+//     println!("Ending stock qty: {:?}", stock_qty);
+//     println!("Ending fiat_total: {:?}", fiat_total);
+
+// 	Ok(())
+// }
 
 struct RSITrader {
     rsi: RelativeStrengthIndex,
@@ -84,28 +143,47 @@ impl RSITrader {
                 overbought: 70,
                 oversold: 30,
                 in_position: false,
-            })
+            }),
         }
     }
 }
 
-impl Next<f64> for RSITrader {
-    type Output = Option<MarketActionType>;
-
-    fn next(&mut self, close: f64) -> Self::Output {
-        let rsi = self.rsi.next(close);
+impl Trade for &mut RSITrader {
+    fn trade(&mut self, data_item: DataItem) -> Option<MarketAction> {
+        let rsi = self.rsi.next(data_item.close());
         self.count = self.count + 1;
 
         if self.count > self.period {
             if (rsi < self.oversold as f64) && !self.in_position {
                 self.in_position = true;
-                return Some(MarketActionType::Buy)
+                return Some(MarketAction::Buy);
             } else if (rsi > self.overbought as f64) && self.in_position {
                 self.in_position = false;
-                return Some(MarketActionType::Sell)
+                return Some(MarketAction::Sell);
             }
         }
 
         None
     }
 }
+
+// impl Next<f64> for RSITrader {
+//     type Output = Option<MarketAction>;
+
+//     fn next(&mut self, close: f64) -> Self::Output {
+//         let rsi = self.rsi.next(close);
+//         self.count = self.count + 1;
+
+//         if self.count > self.period {
+//             if (rsi < self.oversold as f64) && !self.in_position {
+//                 self.in_position = true;
+//                 return Some(MarketAction::Buy)
+//             } else if (rsi > self.overbought as f64) && self.in_position {
+//                 self.in_position = false;
+//                 return Some(MarketAction::Sell)
+//             }
+//         }
+
+//         None
+//     }
+// }
--- src/record.rs
+++ src/record.rs
@@ -5,8 +5,8 @@ pub struct Record {
     date: String,
     pub open: f64,
     pub close: f64,
-	high: f64,
-    low: f64,
-	volume: f64,
-	adj_close: f64,
+    pub high: f64,
+    pub low: f64,
+    pub volume: f64,
+    adj_close: f64,
 }
